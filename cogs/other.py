57a427b34ddd407c1cf734d2add83ba3609a035f
Sophia Wang <s.sophi.w@gmail.com>
2021-07-20 20:20:44 -0400

    A little more cleanup

--- cogs/other.py
+++ cogs/other.py
@@ -15,15 +15,15 @@ class Other(commands.Cog):
 
         # Loads opus, required for voice
         if not discord.opus.is_loaded():
-            discord.opus.load_opus(ctypes.util.find_library('opus'))
+            discord.opus.load_opus(ctypes.util.find_library("opus"))
 
-        @bot.listen('on_message_delete')
+        @bot.listen("on_message_delete")
         async def on_message_delete(msg):
             if msg.author.bot:
                 return
             self.del_snipes[msg.channel.id] = msg
 
-        @bot.listen('on_message_edit')
+        @bot.listen("on_message_edit")
         async def on_message_edit(before, after):
             if before.author.bot:
                 return
@@ -35,69 +35,69 @@ class Other(commands.Cog):
         fut.result()
 
     @commands.command(
-        aliases=['s'],
-        help='Gets the most recently deleted message and shows it.',
-        brief='Deleted message snipe',
-        usage='owo s'
+        aliases=["s"],
+        help="Gets the most recently deleted message and shows it.",
+        brief="Deleted message snipe",
+        usage="owo s"
     )
     async def snipe(self, ctx):
         try:
             snipe = self.del_snipes[ctx.channel.id]
         except KeyError:
-            return await ctx.send('No snipes in this channel!')
+            return await ctx.send("No snipes in this channel!")
         emb = discord.Embed(color=self.bot_color, description=snipe.content, timestamp=snipe.created_at)
         emb.set_author(name=str(snipe.author), icon_url=snipe.author.avatar_url)
         await ctx.send(embed=emb)
 
     @commands.command(
-        aliases=['editsnipe', 'es'],
-        help='Gets the most recently edited message and shows that message both before and after the edit.',
-        brief='Edit snipe in a channel',
-        usage='owo es'
+        aliases=["editsnipe", "es"],
+        help="Gets the most recently edited message and shows that message both before and after the edit.",
+        brief="Edit snipe in a channel",
+        usage="owo es"
     )
     async def edit_snipe(self, ctx):
         try:
             snipe = self.edit_snipes[ctx.channel.id]
         except KeyError:
-            return await ctx.send('No snipes in this channel!')
+            return await ctx.send("No snipes in this channel!")
         emb = discord.Embed(color=self.bot_color, timestamp=snipe[1].created_at)
-        emb.add_field(name='Before', value=snipe[0].content, inline=False)
-        emb.add_field(name='After', value=snipe[1].content, inline=False)
+        emb.add_field(name="Before", value=snipe[0].content, inline=False)
+        emb.add_field(name="After", value=snipe[1].content, inline=False)
         emb.set_author(name=str(snipe[0].author), icon_url=snipe[0].author.avatar_url)
         await ctx.send(embed=emb)
 
     @commands.command(
-        help='Joins a vc, plays Never Gonna Give You Up, and disconnects. If a vc isn\'t specified and the person who'
-             'called the command is in a vc, the bot joins that vc.',
-        brief='Rickroll',
-        usage='owo imbored [vc]'
+        help="Joins a vc, plays Never Gonna Give You Up, and disconnects. If a vc isn't specified and the person who"
+             "called the command is in a vc, the bot joins that vc.",
+        brief="Rickroll",
+        usage="owo imbored [vc]"
     )
     async def imbored(self, ctx, channel: discord.VoiceChannel = None):
         if channel is None:
             try:
                 await ctx.author.voice.channel.connect()
             except AttributeError:
-                await ctx.channel.send('You must be in a vc to use this command')
+                await ctx.channel.send("You must be in a vc to use this command")
                 return
             except discord.errors.ClientException:
-                await ctx.channel.send('The bot is already connected to a vc')
+                await ctx.channel.send("The bot is already connected to a vc")
         else:
             await channel.connect()
         vc = get(self.bot.voice_clients)
-        vc.play(discord.FFmpegPCMAudio(executable='/usr/local/bin/ffmpeg', source='rickroll.mp3'),
+        vc.play(discord.FFmpegPCMAudio(executable="/usr/local/bin/ffmpeg", source="rickroll.mp3"),
                 after=lambda e: self.after(vc))
 
     @commands.command(
-        help='Gives specifics about commands and categories, in an all new ~fancy~ format. Welcome to The Warden 2.0!',
-        brief='Gives specifics about commands and categories',
-        usage='owo help [command/category]'
+        help="Gives specifics about commands and categories, in an all new ~fancy~ format. Welcome to The Warden 2.0!",
+        brief="Gives specifics about commands and categories",
+        usage="owo help [command/category]"
     )
     async def help(self, ctx, cmd: str = None):
         if not cmd:
             embed = discord.Embed(
-                title='Commands and Categories',
-                description='\nUse owo help [command] or owo help [category] to find out more about a specific command '
-                            'or category.',
+                title="Commands and Categories",
+                description="\nUse owo help [command] or owo help [category] to find out more about a specific command "
+                            "or category.",
                 color=self.bot_color)
 
             for cog in self.bot.cogs:
@@ -105,37 +105,37 @@ class Other(commands.Cog):
                 cmds = [c.name for c in cog.get_commands() if not c.hidden]
                 if cmds:
                     embed.add_field(
-                        name=f'__{cog.qualified_name}__', value=f"`Commands:` {', '.join(cmds)}", inline=True)
+                        name=f"__{cog.qualified_name}__", value=f"`Commands:` {', '.join(cmds)}", inline=True)
             await ctx.send(embed=embed)
         else:
             if command := self.bot.get_command(cmd.lower()):
-                command_embed = discord.Embed(title=f'Command: {command.name}',
-                                              description=f'**Usage**\n```ini\n{command.usage}```\n'
-                                                          f'**Description:**\n```ini\n{command.help}```',
+                command_embed = discord.Embed(title=f"Command: {command.name}",
+                                              description=f"**Usage**\n```ini\n{command.usage}```\n"
+                                                          f"**Description:**\n```ini\n{command.help}```",
                                               color=self.bot_color)
-                command_embed.set_footer(text='<> = required param, [] = optional param')
+                command_embed.set_footer(text="<> = required param, [] = optional param")
                 return await ctx.send(embed=command_embed)
 
             for cog in self.bot.cogs:
                 if str(cog).lower() == str(cmd).lower():
                     cog = self.bot.get_cog(cog)
-                    cog_embed = discord.Embed(title=f'Category: {cog.qualified_name}',
-                                              description=f'**Description:**\n```ini\n{cog.description}```',
+                    cog_embed = discord.Embed(title=f"Category: {cog.qualified_name}",
+                                              description=f"**Description:**\n```ini\n{cog.description}```",
                                               color=self.bot_color)
                     for c in cog.get_commands():
                         if not c.hidden:
-                            cog_embed.add_field(name=f'__{c.name}__', value=c.brief, inline=False)
+                            cog_embed.add_field(name=f"__{c.name}__", value=c.brief, inline=False)
                     return await ctx.send(embed=cog_embed)
             else:
-                await ctx.send('What\'s that?')
+                await ctx.send("What's that?")
 
     @commands.command(
-        brief='Tells you the latency',
-        usage='owo ping',
-        help=' It tells you the Discord WebSocket protocol latency. In other words, it says how long it takes for the '
-             'bot and discord to communicate in milliseconds. IT DOES NOT VARY DEPENDING ON WHO USES THE COMMAND')
+        brief="Tells you the latency",
+        usage="owo ping",
+        help="It tells you the Discord WebSocket protocol latency. In other words, it says how long it takes for the "
+             "bot and discord to communicate in milliseconds. IT DOES NOT VARY DEPENDING ON WHO USES THE COMMAND")
     async def ping(self, ctx):
-        await ctx.send(f'Pong! {round(self.bot.latency * 1000, 2)} ms')
+        await ctx.send(f"Pong! {round(self.bot.latency * 1000, 2)} ms")
 
 
 def setup(bot):
--- main.py
+++ main.py
@@ -4,17 +4,18 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-bot = commands.Bot(command_prefix=('owo ', 'uwu '))
-bot.remove_command('help')
+TOKEN = os.getenv("DISCORD_TOKEN")
+bot = commands.Bot(command_prefix=("owo ", "uwu "))
+bot.remove_command("help")
 
 
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'you'))
-    print(f'We have logged in as {bot.user}')
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"you"))
+    print(f"We have logged in as {bot.user}")
 
 
+# Abandoned idea for making the bot able to join other servers
 @bot.event
 async def on_guild_join(guild):
     general = guild.system_channel
@@ -27,34 +28,31 @@ async def on_guild_join(guild):
     await general.send(embed=embed)
 
 
+error_message = {commands.CommandNotFound: "That command doesn't exist",
+                 commands.BotMissingPermissions: "I don't have the permissions needed to run this command",
+                 commands.MissingRole: "You don't have the role(s) needed to use this command",
+                 commands.BadArgument: "Unexpected argument (check your capitalization and parameter order)",
+                 commands.MissingRequiredArgument: f"Missing required argument: `{error.param}`. "
+                                                   f"If you think you included this, check the order",
+                 commands.TooManyArguments: "Too many arguments",
+                 commands.CheckFailure: "You don't have the permissions needed to use this command",
+                 AttributeError: "It's probably due to a spelling error somewhere. Please notify Saph#6803"
+}
+
+
 # Very bad error handling
+# Update: Slightly less bad error handling
 @bot.event
 async def on_command_error(ctx, error):
-    description = 'Error: '
-    if isinstance(error, commands.CommandNotFound):
-        description += 'That command doesn\'t exist'
-    elif isinstance(error, commands.BotMissingPermissions):
-        description += 'I don\'t have the permissions needed to run this command'
-    elif isinstance(error, commands.MissingRole):
-        description += 'You don\'t have the role(s) needed to use this command'
-    elif isinstance(error, commands.BadArgument):
-        description += 'Unexpected argument (check your capitalization and parameter order)'
-    elif isinstance(error, commands.MissingRequiredArgument):
-        description += f'Missing required argument: `{error.param}`. If you think you included this, check the order'
-    elif isinstance(error, commands.TooManyArguments):
-        description += 'Too many arguments'
-    elif isinstance(error, AttributeError):
-        description += 'It\'s probably due to a spelling error somewhere. Please notify Saph#6803'
-    elif isinstance(error, commands.CheckFailure):
-        description += 'You don\'t have the permissions needed to use this command'
-    else:
+    try:
+        description = "Error: "+error_message[error]
+    except KeyError:
         print(error)
         await ctx.channel.send("<@363690578950488074>")
-        description = 'An unexpected error occurred: '+str(error)
-    await ctx.channel.send(embed=discord.Embed(description=description,
-                                               color=discord.Color.from_rgb(214, 11, 11)))
+        description = "An unexpected error occurred: "+str(error)
+    await ctx.channel.send(embed=discord.Embed(description=description, color=discord.Color.from_rgb(214, 11, 11)))
 
 # bot.load_extension("cogs.nsfw_detect")
-bot.load_extension('cogs.moderation')
-bot.load_extension('cogs.other')
+bot.load_extension("cogs.moderation")
+bot.load_extension("cogs.other")
 bot.run(TOKEN)
